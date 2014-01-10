7646b0ae26ee9543e81324a6155f6536165a63f6
dineshkumar-cse <dineshkumar_cse@hotmail.com>
2014-01-10 14:27:51 +0530

    Sample Spring Application

---
+++ main/java/com/springapp/mvc/HomeController.java
@@ -0,0 +1,12 @@
+package com.springapp.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by dineshkb on 1/10/14.
+ */
+@Controller
+@RequestMapping
+public class HomeController {
+}
